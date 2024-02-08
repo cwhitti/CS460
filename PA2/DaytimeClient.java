6e4ab1a5e2e999e8de0267fe33b6532c7d53af05
pehilbert <pehilbert03@gmail.com>
2024-02-07 19:30:03 -0700

    Made the client able to connect to NIST server

--- PA2/DaytimeClient.java
+++ PA2/DaytimeClient.java
@@ -1,13 +1,26 @@
+import java.io.IOException;
 import java.net.Socket;
+import java.io.InputStream;
 
 public class DaytimeClient 
     {
      static Socket socket;
-     String SERVER_HOST = "time.nist.gov";
-     int PORT = 13;
+     static String SERVER_HOST = "time.nist.gov";
+     static int PORT = 13;
+     static char OTM = '*';
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
         {
+         Socket socket = new Socket(SERVER_HOST, PORT);
          
+         if (socket.isConnected())
+            {
+             System.out.println("Connected successfully to " + SERVER_HOST + " on port " + PORT);
+             socket.close();
+            }
+         else
+            {
+             System.out.println("Failed to connect to " + SERVER_HOST + " on port " + PORT);
+            }
         }
     }
