02787fa76336728bbc4f7eaf776a3eed65a9dbd6
cwhitti <112997645+cwhitti@users.noreply.github.com>
2024-02-07 20:05:24 -0700

    Create server.h

---
+++ PA2/Part_1/server.h
@@ -0,0 +1,26 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/* Function prototypes */
+void* handle_client(void* arg);
+char* get_ip_address(const char *string);
+
+/* Preprocessor directives */
+#define SERVER_ADDR "time.nist.gov"
+#define PORT 13              // port the server will listen on
+
+#define FALSE 0
+#define TRUE !FALSE
+
+#define NUM_CONNECTIONS 5       // number of pending connections in the connection queue
+
+#define ASTERISK '*'
+#define BUFFER_SIZE 1024
