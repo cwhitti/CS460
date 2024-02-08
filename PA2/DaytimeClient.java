6f59a71edf1ff50a010578e8187a585c2b732c87
pehilbert <pehilbert03@gmail.com>
2024-02-07 19:11:30 -0700

    Set up main function

--- PA2/DaytimeClient.java
+++ PA2/DaytimeClient.java
@@ -1,3 +1,13 @@
-public class DaytimeClient {
-    
-}
+import java.net.Socket;
+
+public class DaytimeClient 
+    {
+     static Socket socket;
+     String SERVER_HOST = "time.nist.gov";
+     int PORT = 13;
+
+    public static void main(String[] args)
+        {
+         
+        }
+    }
