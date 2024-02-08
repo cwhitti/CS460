3e94dada2ce7927135e6054ef6714681dca3db81
pehilbert <pehilbert03@gmail.com>
2024-02-07 17:01:46 -0700

    Added folde for PA2 with sample code

---
+++ PA2/EchoServer.java
@@ -0,0 +1,57 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class EchoServer {
+
+    private static ServerSocket serverSocket;
+    private static int port;
+    private static int SERVER_PORT = 23657;
+
+
+    public EchoServer(int port) {
+        try {
+            EchoServer.port = port;
+            EchoServer.serverSocket = new ServerSocket(port);
+        } catch (IOException ex) {
+            Logger.getLogger(EchoServer.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Error starting server on port " + port);
+            System.exit(1);
+        }
+
+        EchoServer.port = port;
+    }
+
+    
+    public void runServerLoop() throws IOException {
+
+	while (true) {
+            System.out.println("Waiting for connections on port #" + port); 
+            
+            // if we subclass from Thread 
+            // (new EchoThread(serverSocket.accept())).start();
+            
+            // if we have a runnable
+            (new Thread(new EchoThread(serverSocket.accept()))).start();
+            
+            /*
+            // the above is equivalent with the below.
+            // to avoid race conditions, however, we don't do it that way
+            Socket clientSocket = serverSocket.accept();
+            EchoThread echoThreadRunnable = new EchoThread(clientSocket);
+            Thread echoThread = new Thread(echoThreadRunnable);
+            echoThread.start();
+            */
+	}
+    }
+
+    public static void main(String args[]) throws Exception {
+        // create instance of echo server
+        // note that hardcoding the port is bad, here we do it just for simplicity reasons
+        EchoServer echoServer = new EchoServer(SERVER_PORT);
+
+        // fire up server loop
+        echoServer.runServerLoop();
+    }
+}
