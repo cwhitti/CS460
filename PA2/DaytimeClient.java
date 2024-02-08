ff4ec0e038fbdefa5ade790d36878e86b993ba46
pehilbert <pehilbert03@gmail.com>
2024-02-07 20:03:33 -0700

    IT WORKS LETS GO

--- PA2/DaytimeClient.java
+++ PA2/DaytimeClient.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 import java.net.Socket;
-import java.io.InputStream;
+import java.io.DataInputStream;
 
 public class DaytimeClient 
     {
@@ -12,15 +12,34 @@ public class DaytimeClient
     public static void main(String[] args) throws IOException
         {
          Socket socket = new Socket(SERVER_HOST, PORT);
+         DataInputStream fromServer = new DataInputStream(socket.getInputStream());
+         byte[] serverData = new byte[50];
+         int index;
          
          if (socket.isConnected())
             {
              System.out.println("Connected successfully to " + SERVER_HOST + " on port " + PORT);
-             socket.close();
             }
          else
             {
              System.out.println("Failed to connect to " + SERVER_HOST + " on port " + PORT);
             }
+         
+         System.out.println("Data read from server:");
+        
+         // read first character of input
+         fromServer.readFully(serverData);
+         index = 0;
+
+         while (serverData[index] != OTM)
+            {
+             // print read character
+             System.out.print((char)serverData[index]);
+
+             // read the next character
+             index++;
+            }
+
+         socket.close();
         }
     }
