71a0c9e955290561322f7a37374b315c2256adc2
pehilbert <pehilbert03@gmail.com>
2024-02-22 10:14:37 -0700

    Set up correct addresses

--- Projects/PA3/Server/server.h
+++ Projects/PA3/Server/server.h
@@ -27,12 +27,9 @@ void* handle_client(void* arg);
 void get_current_time( char *outStr );
 
 /* Preprocessor directives */
-#define SERVER_ADDR "hwsrv-1126936.hostwindsdns.com"
+#define THIS_ADDR "hwsrv-1126936.hostwindsdns.com"
+#define REMOTE_ADDR "time.nist.gov"
 #define PORT 13
-
-/* Localhost directives */
-//#define SERVER_ADDR "localhost"
-//#define PORT 8080              // port the server will listen on
 
 #define FALSE 0
 #define TRUE !FALSE
