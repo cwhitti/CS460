cf0e48809e43e0be93a1a958d6c6ef8415e6818b
pehilbert <pehilbert03@gmail.com>
2024-03-20 10:24:31 -0700

    Set up Step 1 files

---
+++ Projects/PA4/Step_1/server-step1.no-frills.c
@@ -0,0 +1,196 @@
+/*
+PA 4
+Peter Hilbert
+*/
+
+#include "server-step1.no-frills.h"
+
+
+/* ************************************************************************* */
+/* MAIN                                                                      */
+/* ************************************************************************* */
+
+int main(int argc, char** argv)
+{
+    int server_socket;                 // descriptor of server socket
+    struct sockaddr_in server_address; // for naming the server's listening socket
+    int yes = 1;
+
+
+    // ----------------------------------------------------------
+    // ignore SIGPIPE, sent when client disconnected
+    // ----------------------------------------------------------
+    signal(SIGPIPE, SIG_IGN);
+
+    // ----------------------------------------------------------
+    // create unnamed network socket for server to listen on
+    // ----------------------------------------------------------
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // lose the pesky "Address already in use" error message
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    // ----------------------------------------------------------
+    // bind the socket
+    // ----------------------------------------------------------
+    server_address.sin_family      = AF_INET;           // accept IP addresses
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY); // accept clients on any interface
+    server_address.sin_port        = htons(PORT);       // port to listen on
+
+    // binding unnamed socket to a particular port
+    if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) != 0)
+    {
+        perror("Error binding socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // ----------------------------------------------------------
+    // listen on the socket
+    // ----------------------------------------------------------
+    if (listen(server_socket, NUM_CONNECTIONS) != 0)
+    {
+        perror("Error listening on socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // ----------------------------------------------------------
+    // server loop
+    // ----------------------------------------------------------
+    while (TRUE)
+    {
+
+        // accept connection to client
+        int client_socket = accept(server_socket, NULL, NULL);
+        printf("\nServer with PID %d: accepted client\n", getpid());
+
+        // create thread to handle the client's request
+        // note that this is a naive approach, i.e. there are race conditions
+        // for now this is okay, assuming low load
+        pthread_t thread;
+        if (pthread_create(&thread, NULL, handle_client, (void*)&client_socket) != 0)
+        {
+            perror("Error creating thread");
+            exit(EXIT_FAILURE);
+        }
+
+        // detach the thread so that we don't have to wait (join) with it to reclaim memory.
+        // memory will be reclaimed when the thread finishes.
+        if (pthread_detach(thread) != 0)
+        {
+            perror("Error detaching thread");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+
+/* ************************************************************************* */
+/* handle client                                                             */
+/* ************************************************************************* */
+
+void* handle_client(void* arg)
+{
+    int client_socket = *((int*)arg);   // the socket connected to the client
+    char  outStr[HUGE_STR_LEN];
+
+    // get time
+    get_current_time( outStr );
+    
+    //printf("String to send to client: %s\n", outStr);
+
+    // send result back to client
+    write(client_socket, &outStr, sizeof(outStr));
+
+    // cleanup
+    if (close(client_socket) == -1)
+    {
+        perror("Error closing socket");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        printf("Closed socket to client, exit\n");
+    }
+
+    pthread_exit(NULL);
+}
+
+/* ************************************************************************* */
+/* handle client                                                             */
+/* ************************************************************************* */
+
+void get_current_time( char *outStr )
+{
+    // initialize variables
+    int proxySocket;
+    char tempStr[HUGE_STR_LEN];
+    char rserverIp[INET_ADDRSTRLEN];
+    struct sockaddr_in rserverAddr;
+    
+    // get ip address of remote server
+    get_ip_address(REMOTE_ADDR, rserverIp);
+    
+    // create the proxy socket
+    proxySocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // configure socket address struct
+    rserverAddr.sin_family = AF_INET;
+    rserverAddr.sin_addr.s_addr = inet_addr(rserverIp);
+    rserverAddr.sin_port = htons(PORT);
+
+    // connect to time server
+    if ( connect(proxySocket, (struct sockaddr*)&rserverAddr, 
+                                    sizeof(rserverAddr) ) == -1 )
+        {
+         perror("Failed to connect to remote time server");
+         exit(EXIT_FAILURE);
+        } 
+
+    // read time info into the temp string
+    read(proxySocket, &tempStr, sizeof(tempStr));
+
+    //printf("Time string in function: %s\n", tempStr);
+    
+    // copy temp string to output string
+    strncpy(outStr, tempStr, HUGE_STR_LEN);
+}
+
+void get_ip_address(const char *string, char *ip_string)
+{
+  struct addrinfo *headPtr, *wkgPtr;
+  struct sockaddr_in *ipv4 = NULL;
+  void *addr;
+  char temp[INET_ADDRSTRLEN];
+
+  //hints.ai_family = AF_INET; // AF_INET means IPv4 only
+
+  if ( getaddrinfo( string, NULL, NULL, &headPtr ) == 0 )
+  {
+    // Loop through all the results and get the IP address
+    for (wkgPtr = headPtr; wkgPtr != NULL; wkgPtr = wkgPtr->ai_next)
+    {
+      ipv4 = (struct sockaddr_in *)wkgPtr->ai_addr;
+      addr = &( ipv4->sin_addr );
+
+      // Convert the IP to a string
+      inet_ntop( wkgPtr->ai_family, addr, temp, sizeof temp);
+    }
+    // Free the linked list
+    freeaddrinfo( headPtr );
+
+    // Copy the IP address string
+    strncpy(ip_string, temp, INET_ADDRSTRLEN);
+  }
+  else
+  {
+    perror("Error resolving nostname.");
+    exit(EXIT_FAILURE);
+  }
+ }
