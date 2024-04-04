4b8fc7c320d26100c0b20b81f11a2506ddc4ab5d
cwhitti <112997645+cwhitti@users.noreply.github.com>
2024-04-03 20:11:44 -0700

    Created chat_node.h files

---
+++ Projects/PA5/Part_2/Client/src/chat_node.h
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define NAME_LEN 16
+
+// ChatNode w/ IP, Port, logical name
+typedef struct chat_node_struct
+{
+  unsigned int ip;
+  unsigned short int port;
+  char name[NAME_LEN];
+} ChatNode;
+
+// ChatNodeListElement for building a list
+typedef struct chat_node_list_element_struct
+{
+  ChatNode chat_node;
+  struct chat_node_list_element_struct* next;
+} ChatNodeListElement;
+
+// ChatNodeList
+typedef struct chat_node_list_struct
+{
+  ChatNodeListElement* first;
+  ChatNodeListElement* last;
+} ChatNodeList;
+
+// create chat node from given data
+ChatNode* createChatNodeFromData( unsigned int ip, unsigned short int port,
+                                                    char* name);
+
+// create a chat node list
+ChatNodes* initializeChatNodeList( void );
+
+void addChatNodeToList( ChatNodeList* chatNodeList, ChatNode* chatNode );
+bool removeNodeFromList( ChatNodeList* chatNodeList, ChatNode *chatNode);
+
+bool compareChatNodes( ChatNode* first, ChatNode* second );
+
+#endif /* chat_node.h */
---
+++ Projects/PA5/Part_2/Server/src/chat_node.c
@@ -0,0 +1,50 @@
+/*
+  Funciton: createChatNodeFromData
+
+  Purpose: Takes client data and creates a node from it
+*/
+ChatNode* createChatNodeFromData( unsigned int ip, unsigned short int port,
+                                                    char* name)
+{
+  return NULL; // temp stub
+}
+
+/*
+  Funciton: initializeChatNodeList
+
+  Purpose: Initializes list of chat nodes
+*/
+ChatNodes* initializeChatNodeList( void )
+{
+  return NULL; // temp stub
+}
+
+/*
+  Funciton: addChatNodeToList
+
+  Purpose: Adds a chatNode to a list
+*/
+void addChatNodeToList( ChatNodeList* chatNodeList, ChatNode* chatNode )
+{
+
+}
+
+/*
+  Funciton: removeNodeFromList
+
+  Purpose: Removes a chatNode to a list, returns success bool
+*/
+bool removeNodeFromList( ChatNodeList* chatNodeList, ChatNode *chatNode)
+{
+  return false; // temp stub
+}
+
+/*
+  Funciton: compareChatNodes
+
+  Purpose: Compares two chat nodes, returns success
+*/
+bool compareChatNodes( ChatNode* first, ChatNode* second )
+{
+  return false; // temp stub
+}
